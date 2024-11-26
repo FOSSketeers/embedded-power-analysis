3b0ca457770585536b6b9a01abfaf4df7ad53bd9
div72 <noreply@div72.xyz>
2024-11-26 12:20:56 +0300

    scripts.visualize: add timing and efficiency plots

--- visualize.py
+++ visualize.py
@@ -33,11 +33,33 @@ def main() -> None:
         level_group = data.loc[~data["D0-D7"].isin((0, 15))].groupby("D0-D7")
         # We're supplying 5V, every data point is for 0.01ms(10^-5). Current is micro(10^-6)amps. Turning uA -> A, ms -> h, end result -> Wh.
         total_usages = level_group["Current(uA)"].agg("sum").rename(index=STATES) * 5 / 1_000_000 / 100_000 / 3600
+        print("===== ENERGY USAGES =====")
+        print(total_usages.sort_values())
+
+        # Each row is 0.01ms.
+        timings = level_group["Timestamp(ms)"].count().rename(index=STATES) * 0.01
+        print("===== TIMINGS =====")
+        print(timings.sort_values())
+
+        # Watt / ms?
+        efficiency = (total_usages / timings).rename(index=STATES)
+        print("===== ENERGY EFFICIENCY =====")
+        print(efficiency.sort_values())
 
     with benchmark("Plot"):
-        ax = sns.barplot(x=total_usages.index.rename("States"), y=total_usages.rename("Watt-hours"), hue=total_usages.rename("Watt-hours")).set_title(f"Total Energy Consumption - {sys.argv[1]}")
+        fig, axs = plt.subplots(nrows=3)
 
-    plt.ylim(0, 1.5 * 10 ** -5)
+        consumption_plot = sns.barplot(x=total_usages.index.rename("States"), y=total_usages.rename("Watt-hours"), hue=total_usages.rename("Watt-hours"), ax=axs[0])
+        consumption_plot.set_ylim(0, 1.5 * 10 ** -5)
+        consumption_plot.set_title(f"Total Energy Consumption - {sys.argv[1]}")
+
+        time_plot = sns.barplot(x=timings.index.rename("States"), y=timings.rename("Microseconds"), hue=timings.rename("Microseconds"), ax=axs[1])
+        time_plot.set_ylim(0, 500)
+        time_plot.set_title(f"Total Time Spent - {sys.argv[1]}")
+
+        efficiency_plot = sns.barplot(x=efficiency.index.rename("States"), y=efficiency.rename("Watt-hour per ms"), hue=efficiency.rename("Watt-hour per ms"), ax=axs[2])
+        efficiency_plot.set_title(f"Efficiency - {sys.argv[1]}")
+
     plt.show()
 
 
