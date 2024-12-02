068e8024f62d93f9f526102b66e36abae213ad2f
div72 <noreply@div72.xyz>
2024-12-02 18:55:51 +0300

    visualize: support multiple comps at the same time

--- visualize.py
+++ visualize.py
@@ -1,5 +1,6 @@
 import sys
 import time
+from concurrent.futures import ThreadPoolExecutor
 from contextlib import contextmanager
 
 import matplotlib.pyplot as plt
@@ -22,44 +23,63 @@ def benchmark(title: str):
         print(f"Finished section '{title}' in {end - start:.3f}s.")
 
 
+def load_data(file: str) -> pd.DataFrame:
+    return pd.read_csv(file, usecols=COLS.keys(), dtype=COLS, converters={"D0-D7": lambda s: np.uint8(int(s[::-1], 2))})
+
+
+def process_data(data: pd.DataFrame) -> tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame]:
+    # Skip 0 and 240 for now
+    level_group = data.loc[~data["D0-D7"].isin((0, 15))].groupby("D0-D7")
+    # We're supplying 5V, every data point is for 0.01ms(10^-5). Current is micro(10^-6)amps. Turning uA -> A, ms -> h, end result -> Wh.
+    total_usages = level_group["Current(uA)"].agg("sum").rename(index=STATES) * 5 / 1_000_000 / 100_000 / 3600
+
+    # Each row is 0.01ms.
+    timings = level_group["Timestamp(ms)"].count().rename(index=STATES) * 0.01
+
+    # Watt / ms?
+    efficiency = (total_usages / timings).rename(index=STATES)
+
+    return total_usages, timings, efficiency
+
+
 def main() -> None:
-    with benchmark("Load CSV"):
-        data = pd.read_csv(sys.argv[1], usecols=COLS.keys(), dtype=COLS, converters={"D0-D7": lambda s: np.uint8(int(s[::-1], 2))})
+    datas: list[pd.DataFrame] = []
+    processed_datas: list[tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame]] = []
 
-    data.info()
+    with ThreadPoolExecutor() as executor:
+        with benchmark("Load CSV"):
+            datas.extend(executor.map(load_data, sys.argv[1:]))
 
-    with benchmark("Process data"):
-        # Skip 0 and 240 for now
-        level_group = data.loc[~data["D0-D7"].isin((0, 15))].groupby("D0-D7")
-        # We're supplying 5V, every data point is for 0.01ms(10^-5). Current is micro(10^-6)amps. Turning uA -> A, ms -> h, end result -> Wh.
-        total_usages = level_group["Current(uA)"].agg("sum").rename(index=STATES) * 5 / 1_000_000 / 100_000 / 3600
-        print("===== ENERGY USAGES =====")
-        print(total_usages.sort_values())
-
-        # Each row is 0.01ms.
-        timings = level_group["Timestamp(ms)"].count().rename(index=STATES) * 0.01
-        print("===== TIMINGS =====")
-        print(timings.sort_values())
-
-        # Watt / ms?
-        efficiency = (total_usages / timings).rename(index=STATES)
-        print("===== ENERGY EFFICIENCY =====")
-        print(efficiency.sort_values())
+        with benchmark("Process data"):
+            processed_datas.extend(executor.map(process_data, datas))
 
     with benchmark("Plot"):
-        fig, axs = plt.subplots(nrows=3)
+        fig, axs = plt.subplots(nrows=3, ncols=len(sys.argv[1:]), squeeze=False, sharex="all", sharey="row")
 
-        consumption_plot = sns.barplot(x=total_usages.index.rename("States"), y=total_usages.rename("Watt-hours"), hue=total_usages.rename("Watt-hours"), ax=axs[0])
-        consumption_plot.set_ylim(0, 1.5 * 10 ** -5)
-        consumption_plot.set_title(f"Total Energy Consumption - {sys.argv[1]}")
+        for (column, file), processed_data in zip(enumerate(sys.argv[1:]), processed_datas):
+            total_usages, timings, efficiency = processed_data
 
-        time_plot = sns.barplot(x=timings.index.rename("States"), y=timings.rename("Microseconds"), hue=timings.rename("Microseconds"), ax=axs[1])
-        time_plot.set_ylim(0, 500)
-        time_plot.set_title(f"Total Time Spent - {sys.argv[1]}")
+            print("######### {file} #########")
+            datas[column].info()
+            print("===== ENERGY USAGES =====")
+            print(total_usages.sort_values())
+            print("===== TIMINGS =====")
+            print(timings.sort_values())
+            print("===== ENERGY EFFICIENCY =====")
+            print(efficiency.sort_values())
 
-        efficiency_plot = sns.barplot(x=efficiency.index.rename("States"), y=efficiency.rename("Watt-hour per ms"), hue=efficiency.rename("Watt-hour per ms"), ax=axs[2])
-        efficiency_plot.set_title(f"Efficiency - {sys.argv[1]}")
+            consumption_plot = sns.barplot(x=total_usages.index.rename("States"), y=total_usages.rename("Watt-hours"), hue=total_usages.rename("Watt-hours"), ax=axs[0, column])
+            consumption_plot.set_ylim(0, 1.5 * 10 ** -5)
+            consumption_plot.set_title(f"Total Energy Consumption - {file}")
 
+            time_plot = sns.barplot(x=timings.index.rename("States"), y=timings.rename("Microseconds"), hue=timings.rename("Microseconds"), ax=axs[1, column])
+            time_plot.set_ylim(0, 500)
+            time_plot.set_title(f"Total Time Spent - {file}")
+
+            efficiency_plot = sns.barplot(x=efficiency.index.rename("States"), y=efficiency.rename("Watt-hour per ms"), hue=efficiency.rename("Watt-hour per ms"), ax=axs[2, column])
+            efficiency_plot.set_title(f"Efficiency - {file}")
+
+    print("batashow")
     plt.show()
 
 
